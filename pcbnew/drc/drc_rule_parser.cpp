jey5nd6/kicad

Add DRC tests for text height and text thickness.
Fixes https://gitlab.com/kicad/code/kicad/issues/8683

pcbnew/drc/drc_rule_parser.cpp
@@ -281,6 +281,8 @@ void DRC_RULES_PARSER::parseConstraint( DRC_RULE* aRule )
     case T_hole_to_hole:        constraint.m_Type = HOLE_TO_HOLE_CONSTRAINT;             break;
     case T_courtyard_clearance: constraint.m_Type = COURTYARD_CLEARANCE_CONSTRAINT;      break;
     case T_silk_clearance:      constraint.m_Type = SILK_CLEARANCE_CONSTRAINT;           break;
+    case T_text_height:         constraint.m_Type = TEXT_HEIGHT_CONSTRAINT;              break;
+    case T_text_thickness:      constraint.m_Type = TEXT_THICKNESS_CONSTRAINT;           break;
     case T_track_width:         constraint.m_Type = TRACK_WIDTH_CONSTRAINT;              break;
     case T_annular_width:       constraint.m_Type = ANNULAR_WIDTH_CONSTRAINT;            break;
     case T_disallow:            constraint.m_Type = DISALLOW_CONSTRAINT;                 break;

wenming-ma/kicad-source-mirror

pcbnew/drc/drc_rule_parser.cpp
@@ -272,6 +272,8 @@ void DRC_RULES_PARSER::parseConstraint( DRC_RULE* aRule )
     case T_hole_to_hole:              c.m_Type = HOLE_TO_HOLE_CONSTRAINT;              break;
     case T_courtyard_clearance:       c.m_Type = COURTYARD_CLEARANCE_CONSTRAINT;       break;
     case T_silk_clearance:            c.m_Type = SILK_CLEARANCE_CONSTRAINT;            break;
+    case T_text_height:               c.m_Type = TEXT_HEIGHT_CONSTRAINT;               break;
+    case T_text_thickness:            c.m_Type = TEXT_THICKNESS_CONSTRAINT;            break;
     case T_track_width:               c.m_Type = TRACK_WIDTH_CONSTRAINT;               break;
     case T_annular_width:             c.m_Type = ANNULAR_WIDTH_CONSTRAINT;             break;
     case T_via_diameter:              c.m_Type = VIA_DIAMETER_CONSTRAINT;              break;