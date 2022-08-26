wenming-ma/kicad-source-mirror

commit 24fd85d9ede25ac87efdc9000748d5aa52174d4f

Cross-probing/selection for multiple items (PCB -> SCH)
Fixes https://gitlab.com/kicad/code/kicad/issues/10469

cvpcb/cvpcb_mainframe.cpp
@@ -31,6 +31,7 @@
 #include <kiface_base.h>
 #include <kiplatform/app.h>
 #include <kiway_express.h>
+#include <string_utils.h>
 #include <project/project_file.h>
 #include <macros.h>
 #include <netlist_reader/netlist_reader.h>
@@ -417,8 +418,8 @@ void CVPCB_MAINFRAME::doCloseWindow()
 
     m_modified = false;
 
-    // clear highlight symbol in schematic:
-    SendMessageToEESCHEMA( true );
+    // clear symbol selection in schematic:
+    SendComponentSelectionToSch( true );
 }
 
 
@@ -708,7 +709,7 @@ void CVPCB_MAINFRAME::refreshAfterSymbolSearch( COMPONENT* aSymbol )
         }
     }
 
-    SendMessageToEESCHEMA();
+    SendComponentSelectionToSch();
     DisplayStatus();
 }
 
@@ -881,40 +882,41 @@ bool CVPCB_MAINFRAME::LoadFootprintFiles()
 }
 
 
-void CVPCB_MAINFRAME::SendMessageToEESCHEMA( bool aClearHighligntOnly )
+void CVPCB_MAINFRAME::SendComponentSelectionToSch( bool aClearSelectionOnly )
 {
     if( m_netlist.IsEmpty() )
         return;
 
-    // clear highlight of previously selected symbols (if any):
-    // Selecting a non existing symbol clears any previously highlighted symbols
-    std::string packet = "$CLEAR: \"HIGHLIGHTED\"";
+    std::string command = "$SELECT: ";
 
-    if( Kiface().IsSingle() )
-        SendCommand( MSG_TO_SCH, packet );
-    else
-        Kiway().ExpressMail( FRAME_SCH, MAIL_CROSS_PROBE, packet, this );
+    if( aClearSelectionOnly )
+    {
+        // Sending an empty list means clearing the selection.
+        if( Kiface().IsSingle() )
+            SendCommand( MSG_TO_SCH, command );
+        else
+            Kiway().ExpressMail( FRAME_SCH, MAIL_SELECTION, command, this );
 
-    if( aClearHighligntOnly )
         return;
+    }
 
     int selection = m_symbolsListBox->GetSelection();
 
-    if ( selection < 0 )    // Nothing selected
+    if( selection < 0 ) // Nothing selected
         return;
 
     if( m_netlist.GetComponent( selection ) == nullptr )
         return;
 
-    // Now highlight the selected symbol:
-    COMPONENT* symbol = m_netlist.GetComponent( selection );
+    // Now select the corresponding symbol on the schematic:
+    wxString ref = m_netlist.GetComponent( selection )->GetReference();
 
-    packet = std::string( "$PART: \"" ) + TO_UTF8( symbol->GetReference() ) + "\"";
+    command += wxT( "F" ) + EscapeString( ref, CTX_IPC );
 
     if( Kiface().IsSingle() )
-        SendCommand( MSG_TO_SCH, packet );
+        SendCommand( MSG_TO_SCH, command );
     else
-        Kiway().ExpressMail( FRAME_SCH, MAIL_CROSS_PROBE, packet, this );
+        Kiway().ExpressMail( FRAME_SCH, MAIL_SELECTION, command, this );
 }
 
 
@@ -1069,7 +1071,7 @@ void CVPCB_MAINFRAME::SetSelectedComponent( int aIndex, bool aSkipUpdate )
     {
         m_symbolsListBox->DeselectAll();
         m_symbolsListBox->SetSelection( aIndex );
-        SendMessageToEESCHEMA();
+        SendComponentSelectionToSch();
     }
 
     m_skipComponentSelect = false;

pcbnew/cross-probing.cpp
@@ -36,6 +36,7 @@
 #include <footprint.h>
 #include <pad.h>
 #include <pcb_track.h>
+#include <pcb_group.h>
 #include <zone.h>
 #include <collectors.h>
 #include <eda_dde.h>
@@ -221,11 +222,11 @@ void PCB_EDIT_FRAME::ExecuteRemoteCommand( const char* cmdline )
         renderSettings->SetHighlight( false );
     }
 
-    if( crossProbingSettings.center_on_items && bbox.GetWidth() > 0 && bbox.GetHeight() > 0 )
+    if( crossProbingSettings.center_on_items && bbox.GetWidth() != 0 && bbox.GetHeight() != 0 )
     {
         if( crossProbingSettings.zoom_to_fit )
         {
-            GetToolManager()->GetTool<PCB_SELECTION_TOOL>()->zoomFitCrossProbeBBox( bbox );
+            GetToolManager()->GetTool<PCB_SELECTION_TOOL>()->ZoomFitCrossProbeBBox( bbox );
         }
 
         FocusOnLocation( (wxPoint) bbox.Centre() );
@@ -293,9 +294,108 @@ std::string FormatProbeItem( BOARD_ITEM* aItem )
 }
 
 
-void PCB_EDIT_FRAME::SendMessageToEESCHEMA( BOARD_ITEM* aSyncItem )
+template <typename ItemContainer>
+void collectItemsForSyncParts( ItemContainer& aItems, std::set<wxString>& parts )
 {
-    std::string packet = FormatProbeItem( aSyncItem );
+    for( EDA_ITEM* item : aItems )
+    {
+        switch( item->Type() )
+        {
+        case PCB_GROUP_T:
+        {
+            PCB_GROUP* group = static_cast<PCB_GROUP*>( item );
+
+            collectItemsForSyncParts( group->GetItems(), parts );
+
+            break;
+        }
+        case PCB_FOOTPRINT_T:
+        {
+            FOOTPRINT* footprint = static_cast<FOOTPRINT*>( item );
+            wxString   ref = footprint->GetReference();
+
+            parts.emplace( wxT( "F" ) + EscapeString( ref, CTX_IPC ) );
+
+            break;
+        }
+
+        case PCB_PAD_T:
+        {
+            PAD*       pad = static_cast<PAD*>( item );
+            FOOTPRINT* footprint = static_cast<FOOTPRINT*>( pad->GetParentFootprint() );
+            wxString   ref = footprint->GetReference();
+
+            parts.emplace( wxT( "P" ) + EscapeString( ref, CTX_IPC ) + wxT( "/" )
+                           + EscapeString( pad->GetNumber(), CTX_IPC ) );
+
+            break;
+        }
+
+        default: break;
+        }
+    }
+}
+
+
+void PCB_EDIT_FRAME::SendSelectItemsToSch( const std::deque<EDA_ITEM*>& aItems,
+                                           EDA_ITEM* aFocusItem, bool aForce )
+{
+    std::string command = "$SELECT: ";
+
+    if( aFocusItem )
+    {
+        std::deque<EDA_ITEM*> focusItems = { aFocusItem };
+        std::set<wxString>    focusParts;
+        collectItemsForSyncParts( focusItems, focusParts );
+
+        if( focusParts.size() > 0 )
+        {
+            command += "1,";
+            command += *focusParts.begin();
+            command += ",";
+        }
+        else
+        {
+            command += "0,";
+        }
+    }
+    else
+    {
+        command += "0,";
+    }
+
+    std::set<wxString> parts;
+    collectItemsForSyncParts( aItems, parts );
+
+    if( parts.empty() )
+        return;
+
+    for( wxString part : parts )
+    {
+        command += part;
+        command += ",";
+    }
+
+    command.pop_back();
+
+    if( Kiface().IsSingle() )
+    {
+        SendCommand( MSG_TO_PCB, command );
+    }
+    else
+    {
+        // Typically ExpressMail is going to be s-expression packets, but since
+        // we have existing interpreter of the selection packet on the other
+        // side in place, we use that here.
+        Kiway().ExpressMail( FRAME_SCH, aForce ? MAIL_SELECTION_FORCE : MAIL_SELECTION, command,
+                             this );
+    }
+}
+
+
+void PCB_EDIT_FRAME::SendCrossProbeNetName( const wxString& aNetName )
+{
+    std::string packet = StrPrintf( "$NET: \"%s\"", TO_UTF8( aNetName ) );
 
     if( !packet.empty() )
     {
@@ -314,9 +414,9 @@ void PCB_EDIT_FRAME::SendMessageToEESCHEMA( BOARD_ITEM* aSyncItem )
 }
 
 
-void PCB_EDIT_FRAME::SendCrossProbeNetName( const wxString& aNetName )
+void PCB_EDIT_FRAME::SendCrossProbeItem( BOARD_ITEM* aSyncItem )
 {
-    std::string packet = StrPrintf( "$NET: \"%s\"", TO_UTF8( aNetName ) );
+    std::string packet = FormatProbeItem( aSyncItem );
 
     if( !packet.empty() )
     {
@@ -503,7 +603,7 @@ void PCB_EDIT_FRAME::KiwayMailIn( KIWAY_EXPRESS& mail )
             std::vector<BOARD_ITEM*> items =
                     FindItemsFromSyncSelection( paramStr.substr( modeEnd + 1 ) );
 
-            m_syncingSchToPcbSelection = true; // recursion guard
+            m_probingSchToPcb = true; // recursion guard
 
             if( selectConnections )
             {
@@ -516,7 +616,7 @@ void PCB_EDIT_FRAME::KiwayMailIn( KIWAY_EXPRESS& mail )
                                              static_cast<void*>( &items ) );
             }
 
-            m_syncingSchToPcbSelection = false;
+            m_probingSchToPcb = false;
         }
 
         break;

common/tool/actions.cpp
@@ -667,6 +667,7 @@ TOOL_ACTION ACTIONS::reportBug( "common.SuiteControl.reportBug",
 
 // System-wide selection Events
 
+const TOOL_EVENT EVENTS::PointSelectedEvent( TC_MESSAGE, TA_ACTION, "common.Interactive.pointSelected" );
 const TOOL_EVENT EVENTS::SelectedEvent( TC_MESSAGE, TA_ACTION, "common.Interactive.selected" );
 const TOOL_EVENT EVENTS::UnselectedEvent( TC_MESSAGE, TA_ACTION, "common.Interactive.unselected" );
 const TOOL_EVENT EVENTS::ClearedEvent( TC_MESSAGE, TA_ACTION, "common.Interactive.cleared" );

common/tool/selection.cpp
@@ -37,7 +37,10 @@ void SELECTION::Add( EDA_ITEM* aItem )
     ITER i = std::lower_bound( m_items.begin(), m_items.end(), aItem );
 
     if( i == m_items.end() || *i > aItem )
+    {
         m_items.insert( i, aItem );
+        m_lastAddedItem = aItem;
+    }
 }
 
 
@@ -46,7 +49,12 @@ void SELECTION::Remove( EDA_ITEM* aItem )
     ITER i = std::lower_bound( m_items.begin(), m_items.end(), aItem );
 
     if( !( i == m_items.end() || *i > aItem ) )
+    {
         m_items.erase( i );
+
+        if( aItem == m_lastAddedItem )
+            m_lastAddedItem = nullptr;
+    }
 }
 
 

common/tool/tool_event.cpp
@@ -224,7 +224,8 @@ bool TOOL_EVENT::IsSelectionEvent() const
 {
     return Matches( EVENTS::ClearedEvent )
         || Matches( EVENTS::UnselectedEvent )
-        || Matches( EVENTS::SelectedEvent );
+        || Matches( EVENTS::SelectedEvent )
+        || Matches( EVENTS::PointSelectedEvent );
 }
 
 

cvpcb/cvpcb_mainframe.h
@@ -250,10 +250,10 @@ public:
      * Commands are:
      * $PART: "reference" put cursor on component anchor
      *
-     * @param aClearHighligntOnly use true if the message to send is only "clear highlight"
+     * @param aClearSelectionOnly use true if the message to send is only "clear clear selection"
      *                            (used when exiting CvPcb)
      */
-    void SendMessageToEESCHEMA( bool aClearHighligntOnly = false );
+    void SendComponentSelectionToSch( bool aClearSelectionOnly = false );
 
     /**
      * Get the selected component from the component listbox.

eeschema/sch_edit_frame.cpp
@@ -120,6 +120,7 @@ SCH_EDIT_FRAME::SCH_EDIT_FRAME( KIWAY* aKiway, wxWindow* aParent ) :
 
     m_showBorderAndTitleBlock = true;   // true to show sheet references
     m_supportsAutoSave = true;
+    m_syncingPcbToSchSelection = false;
     m_aboutTitle = _( "KiCad Schematic Editor" );
 
     m_findReplaceDialog = nullptr;
@@ -1762,10 +1763,13 @@ void SCH_EDIT_FRAME::FocusOnItem( SCH_ITEM* aItem )
 
     if( aItem )
     {
-        aItem->SetBrightened();
+        if( !aItem->IsBrightened() )
+        {
+            aItem->SetBrightened();
 
-        UpdateItem( aItem );
-        lastBrightenedItemID = aItem->m_Uuid;
+            UpdateItem( aItem );
+            lastBrightenedItemID = aItem->m_Uuid;
+        }
 
         FocusOnLocation( aItem->GetFocusPosition() );
     }

commit 8d37fb1420432f19145250343cd1b3bc7cd1c9e5

Fix cross-probing from Cvpcb to Eeschema, broken by commit 24fd85d9 Fixes #12293 https://gitlab.com/kicad/code/kicad/issues/12293

cvpcb/cvpcb_mainframe.cpp
@@ -911,7 +911,9 @@ void CVPCB_MAINFRAME::SendComponentSelectionToSch( bool aClearSelectionOnly )
     // Now select the corresponding symbol on the schematic:
     wxString ref = m_netlist.GetComponent( selection )->GetReference();
 
-    command += wxT( "F" ) + EscapeString( ref, CTX_IPC );
+    // The prefix 0,F before the reference is for selecting the symbol
+    // (one can select a pin with a different prefix)
+    command += wxT( "0,F" ) + EscapeString( ref, CTX_IPC );
 
     if( Kiface().IsSingle() )
         SendCommand( MSG_TO_SCH, command );

eeschema/cross-probing.cpp
@@ -840,6 +840,10 @@ void SCH_EDIT_FRAME::KiwayMailIn( KIWAY_EXPRESS& mail )
         std::string prefix = "$SELECT: ";
 
         std::string paramStr = payload.substr( prefix.size() );
+
+        if( paramStr.size() < 2 )   // Empty/broken command: we need at least 2 chars for sync string.
+            break;
+
         std::string syncStr = paramStr.substr( 2 );
 
         bool focusOnFirst = ( paramStr[0] == '1' );