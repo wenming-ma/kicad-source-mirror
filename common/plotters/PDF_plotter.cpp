wenming-ma/kicad-source-mirror

commit 377645927d58b483972f96189eca763b8597fd65

Fix compile warnings

common/plotters/PDF_plotter.cpp
@@ -983,7 +983,7 @@ void PDF_PLOTTER::emitOutlineNode( OUTLINE_NODE* node, int parentHandle, int nex
 
         if( node->children.size() > 0 )
         {
-            fprintf( m_outputFile, "   /Count %lld\n", -1*node->children.size() );
+            fprintf( m_outputFile, "   /Count %zu\n", -1*node->children.size() );
             fprintf( m_outputFile, "   /First %d 0 R\n", node->children.front()->entryHandle );
             fprintf( m_outputFile, "   /Last %d 0 R\n", node->children.back()->entryHandle );
         }

pcbnew/dialogs/dialog_net_inspector.cpp
@@ -481,7 +481,7 @@ public:
                 m_items.begin(), m_items.end(), aNetCode, LIST_ITEM_NETCODE_CMP_LESS() );
 
         if( i == m_items.end() || ( *i )->GetNetCode() != aNetCode )
-            return {};
+            return std::nullopt;
 
         return { i };
     }
@@ -491,7 +491,7 @@ public:
         if( aNet != nullptr )
             return findItem( aNet->GetNetCode() );
         else
-            return {};
+            return std::nullopt;
     }
 
     std::optional<LIST_ITEM_ITER> addItem( std::unique_ptr<LIST_ITEM> aItem )

commit 9a0b35d165bb073e8395a7b43e90a4250e906192

Ensure node count is signed
Fixes a broken commit from 377645927d58b483972f96189eca763b8597fd65

common/plotters/PDF_plotter.cpp
@@ -983,7 +983,7 @@ void PDF_PLOTTER::emitOutlineNode( OUTLINE_NODE* node, int parentHandle, int nex
 
         if( node->children.size() > 0 )
         {
-            fprintf( m_outputFile, "   /Count %zu\n", -1*node->children.size() );
+            fprintf( m_outputFile, "   /Count %zd\n", -1 * node->children.size() );
             fprintf( m_outputFile, "   /First %d 0 R\n", node->children.front()->entryHandle );
             fprintf( m_outputFile, "   /Last %d 0 R\n", node->children.back()->entryHandle );
         }