wenming-ma/kicad-source-mirror

commit 383efd1032f343e4f833c745328a8871cb10c6c3

Better handling of PCB_BITMAP items in a footprint. - Ensure they can be erased in the FP editor - Ensure they cannot be selected in the PCB editor (they are not show) Fixes #13714 https://gitlab.com/kicad/code/kicad/issues/

pcbnew/tools/pcb_selection_tool.cpp
@@ -2240,8 +2240,13 @@ bool PCB_SELECTION_TOOL::itemPassesFilter( BOARD_ITEM* aItem, bool aMultiSelect 
     case PCB_FP_SHAPE_T:
     case PCB_SHAPE_T:
     case PCB_TARGET_T:
-    case PCB_BITMAP_T:
         if( !m_filter.graphics )
+            return false;
+
+        break;
+
+    case PCB_BITMAP_T:
+        if( !m_filter.graphics || !m_isFootprintEditor )
             return false;
 
         break;

pcbnew/footprint.cpp
@@ -641,6 +641,7 @@ void FOOTPRINT::Remove( BOARD_ITEM* aBoardItem, REMOVE_MODE aMode )
     case PCB_FP_DIM_LEADER_T:
     case PCB_FP_SHAPE_T:
     case PCB_FP_TEXTBOX_T:
+    case PCB_BITMAP_T:
         for( auto it = m_drawings.begin(); it != m_drawings.end(); ++it )
         {
             if( *it == aBoardItem )

pcbnew/tools/edit_tool.cpp
@@ -66,6 +66,7 @@ using namespace std::placeholders;
 #include <dialogs/dialog_unit_entry.h>
 #include <board_commit.h>
 #include <zone_filler.h>
+#include <pcb_bitmap.h>
 
 const unsigned int EDIT_TOOL::COORDS_PADDING = pcbIUScale.mmToIU( 20 );
 
@@ -1863,6 +1864,21 @@ void EDIT_TOOL::DeleteItems( const PCB_SELECTION& aItems, bool aIsCut )
             parent->Remove( textbox );
             break;
         }
+
+        case PCB_BITMAP_T:
+            if( IsFootprintEditor() )
+            {
+                PCB_BITMAP* fp_bitmap = static_cast<PCB_BITMAP*>( item );
+                FOOTPRINT*  parent = static_cast<FOOTPRINT*>( item->GetParent() );
+
+                m_commit->Modify( parent );
+                getView()->Remove( fp_bitmap );
+                parent->Remove( fp_bitmap );
+            }
+            else
+                m_commit->Remove( item );
+
+            break;
 
         case PCB_PAD_T:
             if( IsFootprintEditor() || frame()->GetPcbNewSettings()->m_AllowFreePads )

commit f1ecb0c033f7ca4d167e37da57e981ae8734ab47

Re-allows selection of bitmaps not owned by a footprint. It was broken by my commit 383efd10 Fixes #13747 https://gitlab.com/kicad/code/kicad/issues/13747

pcbnew/tools/pcb_selection_tool.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2013-2017 CERN
- * Copyright (C) 2018-2022 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2018-2023 KiCad Developers, see AUTHORS.txt for contributors.
  * @author Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
  * @author Maciej Suminski <maciej.suminski@cern.ch>
  *
@@ -2246,8 +2246,16 @@ bool PCB_SELECTION_TOOL::itemPassesFilter( BOARD_ITEM* aItem, bool aMultiSelect 
         break;
 
     case PCB_BITMAP_T:
-        if( !m_filter.graphics || !m_isFootprintEditor )
+        // a bitmap living in a footprint must not be selected inside the board editor
+        if( !m_filter.graphics )
             return false;
+
+        if( !m_isFootprintEditor )
+        {
+            if( dynamic_cast<FOOTPRINT*>( aItem->GetParent() ) )
+                return false;
+        }
+
 
         break;
 