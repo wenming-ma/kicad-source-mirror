wenming-ma/kicad-source-mirror

commit 1968e708c55332e47ff898aaa79f8a887373e095

Refactor: Move static to be inside ZONE_SETTINGS

pcbnew/zone_settings.cpp
@@ -196,6 +196,14 @@ const static wxSize LAYER_BITMAP_SIZE( 24, 16 );
 const static wxSize CHECKERBOARD_SIZE( 8, 8 );
 
 
+static const ZONE_SETTINGS& GetDefaultSettings()
+{
+    static ZONE_SETTINGS defaultSettings;
+
+    return defaultSettings;
+}
+
+
 // A helper for setting up a dialog list for specifying zone layers.  Used by all three
 // zone settings dialogs.
 void ZONE_SETTINGS::SetupLayersList( wxDataViewListCtrl* aList, PCB_BASE_FRAME* aFrame,

pcbnew/teardrop/teardrop.cpp
@@ -54,9 +54,6 @@ TEARDROP_MANAGER::TEARDROP_MANAGER( BOARD* aBoard, TOOL_MANAGER* aToolManager ) 
 }
 
 
-// Build a zone teardrop
-static ZONE_SETTINGS s_default_settings;      // Use zone default settings for teardrop
-
 ZONE* TEARDROP_MANAGER::createTeardrop( TEARDROP_VARIANT aTeardropVariant,
                                         std::vector<VECTOR2I>& aPoints, PCB_TRACK* aTrack ) const
 {
@@ -64,7 +61,7 @@ ZONE* TEARDROP_MANAGER::createTeardrop( TEARDROP_VARIANT aTeardropVariant,
 
     // teardrop settings are the last zone settings used by a zone dialog.
     // override them by default.
-    s_default_settings.ExportSetting( *teardrop, false );
+    ZONE_SETTINGS::GetDefaultSettings().ExportSetting( *teardrop, false );
 
     // Add zone properties (priority will be fixed later)
     teardrop->SetTeardropAreaType( aTeardropVariant == TD_TYPE_PADVIA ? TEARDROP_TYPE::TD_VIAPAD

pcbnew/zone_settings.h
@@ -144,6 +144,11 @@ public:
     ZONE_SETTINGS& operator << ( const ZONE& aSource );
 
     /**
+     * @return Default ZONE_SETTINGS
+     */
+    static const ZONE_SETTINGS& GetDefaultSettings();
+
+    /**
      * A helper routine for the various zone dialogs (copper, non-copper, keepout).
      * @param aList the wxDataViewListCtrl to populate
      * @param aFrame the parent editor frame

commit bb382564a2b28520edbd478dbd76e40b01ccadc4

Fix build (broken in 1968e708)

pcbnew/zone_settings.cpp
@@ -196,7 +196,7 @@ const static wxSize LAYER_BITMAP_SIZE( 24, 16 );
 const static wxSize CHECKERBOARD_SIZE( 8, 8 );
 
 
-static const ZONE_SETTINGS& GetDefaultSettings()
+const ZONE_SETTINGS& ZONE_SETTINGS::GetDefaultSettings()
 {
     static ZONE_SETTINGS defaultSettings;
 