wenming-ma/kicad-source-mirror

commit 92ac987e06a3ed46e3a6ec16636fe52555324861

Fix use-after-free in Altium importer
If the library already contains a symbol with the same name,
SaveSymbol will replace the existing symbol,
which will invalidate a cached pointer.

eeschema/sch_io/altium/sch_io_altium.cpp
@@ -3053,6 +3053,11 @@ void SCH_IO_ALTIUM::ParsePowerPort( const std::map<wxString, wxString>& aPropert
     {
         libSymbol = powerSymbolIt->second; // cache hit
     }
+    else if( LIB_SYMBOL* alreadyLoaded =
+             m_pi->LoadSymbol( getLibFileName().GetFullPath(), elem.text, m_properties.get() ) )
+    {
+        libSymbol = alreadyLoaded;
+    }
     else
     {
         libSymbol = new LIB_SYMBOL( wxEmptyString );

commit 55d5825015f911aeeb0a491ec18d58751cfb5a5e

Altium schematic import: fix power port styles.
Need to use the symbol name with style, like "VCC_CIRCLE", not "VCC".

Issue was introduced in 92ac987e06a3ed46e3a6ec16636fe52555324861

Fixes https://gitlab.com/kicad/code/kicad/-/issues/18209

eeschema/sch_io/altium/sch_io_altium.cpp
@@ -3356,8 +3356,8 @@ void SCH_IO_ALTIUM::ParsePowerPort( const std::map<wxString, wxString>& aPropert
     {
         libSymbol = powerSymbolIt->second; // cache hit
     }
-    else if( LIB_SYMBOL* alreadyLoaded = m_pi->LoadSymbol( getLibFileName().GetFullPath(),
-                                                           elem.text, m_properties.get() ) )
+    else if( LIB_SYMBOL* alreadyLoaded = m_pi->LoadSymbol( getLibFileName().GetFullPath(), symName,
+                                                           m_properties.get() ) )
     {
         libSymbol = alreadyLoaded;
     }