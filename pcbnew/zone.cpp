wenming-ma/kicad-source-mirror

commit 64c0f1d500f7980956419fa2a96e32538077decc

Cleanup export zone settings for initializing zones (KICAD-EYF).
(cherry picked from commit 79d17fcb6eea20c16076deffc312c643ada81817)

pcbnew/zone.cpp
@@ -85,9 +85,9 @@ ZONE::ZONE( BOARD_ITEM_CONTAINER* aParent ) :
         SetIsRuleArea( true );        // Zones living in footprints have the rule area option
 
     if( aParent->GetBoard() )
-        aParent->GetBoard()->GetDesignSettings().GetDefaultZoneSettings().ExportSetting( *this );
+        aParent->GetBoard()->GetDesignSettings().GetDefaultZoneSettings().ExportSetting( *this, false );
     else
-        ZONE_SETTINGS().ExportSetting( *this );
+        ZONE_SETTINGS().ExportSetting( *this, false );
 
     m_needRefill = false;   // True only after edits.
 }

commit d14fed15fbc14d67e3b2c80aaa133088370c4a58

Fix zones having no net when opening Altium or Legacy PCBs.
At least these importers set the zone net code before zone layers.

64c0f1d500f7980956419fa2a96e32538077decc caused IsOnCopperLayer()
to return false in BOARD_CONNECTED_ITEM::SetNetCode,
so the net code was getting set to 0.

pcbnew/zone.cpp
@@ -77,8 +77,9 @@ ZONE::ZONE( BOARD_ITEM_CONTAINER* aParent ) :
         m_area( 0.0 ),
         m_outlinearea( 0.0 )
 {
-    m_Poly = new SHAPE_POLY_SET();    // Outlines
-    SetLocalFlags( 0 );               // flags temporary used in zone calculations
+    m_Poly = new SHAPE_POLY_SET(); // Outlines
+    SetLocalFlags( 0 );            // flags temporary used in zone calculations
+    SetLayerSet( { F_Cu } );       // Place on a copper layer by default so setting a net is allowed
 
     if( GetParentFootprint() )
         SetIsRuleArea( true );        // Zones living in footprints have the rule area option